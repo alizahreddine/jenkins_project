2bff545e22b5e69bd230c7cfcf0c58f77d88c6a8
Ali <azahreddine@hotmail.com>
2025-11-09 17:38:04 +0200

    test

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+from app import greet
+import unittest
+
+class TestApp(unittest.TestCase):
+    def test_greet(self):
+        self.assertEqual(greet("World"), "Hello, World from Ali Zahreddine!")
+
+if __name__ == "__main__":
+    unittest.main()
